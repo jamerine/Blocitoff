40eccc5384959c25d75d65f66454a03efe34d347
Jason Amerine <amerine.19@gmail.com>
2016-01-28 14:32:25 -0500

    Add username to signup process

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { User.create!(username: "Blocitoff User", email: "amerine.19@gmail.com", password: "password")}
+  # Shoulda tests for name
+  it { should validate_presence_of(:username) }
+  it { should validate_length_of(:username).is_at_least(1) }
+
+  # Shoulda tests for email
+  it { should validate_presence_of(:email) }
 end
