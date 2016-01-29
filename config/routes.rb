39eddb5ac721611a64f621c0b020405096f23596
Jason Amerine <amerine.19@gmail.com>
2016-01-29 10:23:58 -0500

    Add user profile view and route after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
+  devise_for :users
 
   root 'welcome#index'
-  devise_for :users
+
+  resources :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:my_user) { User.create!(username: "TestUserName", email: "test@email.com", password: "password" ) }
+
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, {id: my_user.id}
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, {id: my_user.id}
+      expect(response).to render_template :show
     end
   end
 
